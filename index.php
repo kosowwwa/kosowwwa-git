ba6f388a617d0476ccf247022ff83608b2047621
Erblin Derguti <erblinderguti@gmail.com>
2019-04-05 01:48:11 +0200

    XK on Title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="EN">
   <head>
-    <title>Home - Kosowwwa Digital Agency</title>
+    <title>Home - Kosowwwa Digital Agency - XK</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Kosowwwa Digital Agency">
